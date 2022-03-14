7c95bb6851ae67a829b02bb6f8f3e2f0c24fe14f
TiagoMerc <tmrrosario9@gmail.com>
2022-03-14 10:17:14 -0300

    Criando um contador

--- asserts/js/scripts.js
+++ asserts/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+/*Função incrementar */
+function increment() {
+  currentNumber = currentNumber + 1; 
+  currentNumberWrapper.innerHTML = currentNumber; /*  Html que está dentro dele "innerHTML"*/
+}
+
+/*Função decrementar */
+function decrement() {
+  currentNumber = currentNumber - 1; 
+  currentNumberWrapper.innerHTML = currentNumber;
+}
